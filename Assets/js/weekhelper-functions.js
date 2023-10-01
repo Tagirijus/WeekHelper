3028c516cc0748b8ca833203e0c72847170fe401
Manuel <info@tagirijus.de>
2023-10-01 17:46:05 +0200

    Changed that start of the day is now monday

--- Assets/js/weekhelper-functions.js
+++ Assets/js/weekhelper-functions.js
@@ -2,8 +2,8 @@ var WEEKPATTERN = null;
 
 
 Date.prototype.getWeek = function() {
-	var onejan = new Date(this.getFullYear(),0,1);
-	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+	var firstOfJanuary = new Date(this.getFullYear(), 0, 1);
+	return Math.ceil((((this - firstOfJanuary) / 86400000) + firstOfJanuary.getDay() - 1) / 7);
 }
 
 function getCurrentDateContent() {
