1ae25836c7fca84a9629bb1bfbc998a14b48a890
Manuel <info@tagirijus.de>
2025-12-22 07:22:02 +0100

    automatic planner: improved TimeSlotsDay tests

--- tests/TimeSlotsDayTest.php
+++ tests/TimeSlotsDayTest.php
@@ -11,39 +11,42 @@ use Kanboard\Plugin\WeekHelper\Helper\TimeSlotsDay;
 
 final class TimeSlotsDayTest extends TestCase
 {
+    private static string $config;
+    private static TimeSlotsDay $time_slots;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$config = "6:00-9:00 office\n11:00-13:00 studio\n15:00-17:00";
+        self::$time_slots = new TimeSlotsDay(self::$config);
+    }
+
     public function testtimeSlorsDayInitialize(): void
     {
-        $config = "6:00-9:00 office\n11:00-13:00 studio\n15:00-17:00";
-        $time_slots = new TimeSlotsDay($config);
-
         $this->assertSame(
             0,  # should be the first array key internally
-            $time_slots->nextSlot('office'),
+            self::$time_slots->nextSlot('office'),
             'TimeSlotsDay config was parsed incorrectly, assumingly.'
         );
         $this->assertSame(
             1,
-            $time_slots->nextSlot('studio'),
+            self::$time_slots->nextSlot('studio'),
             'TimeSlotsDay config was parsed incorrectly, assumingly.'
         );
         $this->assertSame(
             900,
-            $time_slots->getStartOfSlot(2),
+            self::$time_slots->getStartOfSlot(2),
             'TimeSlotsDay config was parsed incorrectly, assumingly.'
         );
     }
 
     public function testTimeSlotsDayMethods()
     {
-        $config = "6:00-9:00 office\n11:00-13:00 studio\n15:00-17:00";
-        $time_slots = new TimeSlotsDay($config);
-
         // plan time (30 min) on second slot
-        $slot_key = $time_slots->nextSlot('studio');
-        $time_slots->planTime($slot_key, 30);
+        $slot_key = self::$time_slots->nextSlot('studio');
+        self::$time_slots->planTime($slot_key, 30);
         $this->assertSame(
             690,  # should be 11:30 o'clock
-            $time_slots->getStartOfSlot($slot_key),
+            self::$time_slots->getStartOfSlot($slot_key),
             'Was not able to plan time in TimeSlotsDay for the second time slot.'
         );
 
@@ -51,7 +54,7 @@ final class TimeSlotsDayTest extends TestCase
         // 3 hours / 180 minutes
         $this->assertSame(
             180,
-            $time_slots->getLengthOfSlot(0),
+            self::$time_slots->getLengthOfSlot(0),
             'First time slot is not 3 horus / 180 minutes long.'
         );
 
@@ -63,13 +66,13 @@ final class TimeSlotsDayTest extends TestCase
         // the next free slot is actually of type "studio", but
         // in nextSlot() not type is defined.
         $this->assertTrue(
-            $time_slots->depleteSlot(0),
+            self::$time_slots->depleteSlot(0),
             'Was not able to deplete the first slot.'
         );
-        $slot_key = $time_slots->nextSlot();
+        $slot_key = self::$time_slots->nextSlot();
         $this->assertSame(
             690,
-            $time_slots->getStartOfSlot($slot_key),
+            self::$time_slots->getStartOfSlot($slot_key),
             '11:30 (690 minutes) was not feteched as next start. Either depletion or type fetching failed.'
         );
     }
