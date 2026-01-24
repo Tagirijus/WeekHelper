1c528d68d2732705abd1031da806806bece834a2
Manuel <info@tagirijus.de>
2026-01-24 07:09:41 +0100

    fixed subtasks timer template

--- Template/subtask/timer.php
+++ Template/subtask/timer.php
@@ -3,14 +3,14 @@
 ?>
 <span class="subtask-time-tracking">
     <?php if (! empty($subtask['time_spent'])): ?>
-        <?= $$times->floatToHHMM($subtask['time_spent']) . 'h ' . t('spent') ?>
-        <?= $$times->floatToHHMM($subtask['time_spent']) . 'h ' . t('spent') ?>
+        <?= $times->floatToHHMM($subtask['time_spent']) . 'h ' . t('spent') ?>
+        <?= $times->floatToHHMM($subtask['time_spent']) . 'h ' . t('spent') ?>
     <?php endif ?>
 
     <?php if (! empty($subtask['time_spent']) && ! empty($subtask['time_estimated'])): ?>/<?php endif ?>
 
     <?php if (! empty($subtask['time_estimated'])): ?>
-        <?= $$times->floatToHHMM($subtask['time_estimated']) . 'h ' . t('estimated') ?>
+        <?= $times->floatToHHMM($subtask['time_estimated']) . 'h ' . t('estimated') ?>
     <?php endif ?>
 
     <?php if ($this->user->hasProjectAccess('SubtaskController', 'edit', $task['project_id']) && $subtask['user_id'] == $this->user->getId()): ?>
